9a8ee4f46f14023c72d5423be1896c222f24c793
Dave Knapik <daveknapik@gmail.com>
2012-11-18 12:44:25 -0500

    Initial addition of application files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gigmates::Application.config.secret_token = '669830d13b11a39fad509f01d4fbace2a5dfe0bab7d3d064d192631c707de1273d08ae1245358ee483d28b9d3da7ed5bf7d62e54ad3e4fd64975ecab583fb337'
